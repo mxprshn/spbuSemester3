ecef4e74e2842fe16eccabe16fbbc8ad9c7cc901
mxprshn <mxprshn@gmail.com>
2019-10-29 17:16:31 +0300

    Added class files

---
+++ SimpleFTP/FTPServer/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace FTPServer
+{
+    public class Server
+    {
+        private const int port = 8888;
+
+        public void Start()
+        {
+
+        }
+
+        public void Shutdown()
+        {
+
+        }
+    }
+}
