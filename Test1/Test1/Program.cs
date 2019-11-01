a3b1ea199c14ec73f3e0aff7da2e634d8e74df5f
mxprshn <mxprshn@gmail.com>
2019-11-01 15:17:39 +0300

    Add project files

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Test1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static Server server;
+        private static Client client;
+
+        public static async Task Main(string[] args)
         {
+            switch (args.Length)
+            {
+                case 1:
+                    {
+                        server = new Server(int.Parse(args[0]));
+                        await server.Run();
+                        break;
+                    }
+                case 2:
+                    {
+                        client = new Client(args[0], int.Parse(args[1]));
+                        await client.Run();
+                        break;
+                    }
+            }          
         }
     }
 }
