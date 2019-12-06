7319191cb448e0a97ee625aae00f8d34dd9f52da
mxprshn <mxprshn@gmail.com>
2019-12-06 14:09:55 +0300

    Add project files

---
+++ TicTacToe/TicTacToe/Game.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        private const int MaxMoveCount = 9; 
+        private Player[] players = new Player[] { new Player("crosses", 'X'), new Player("noughts",'O') };
+        private int moveCount;
+        public Player CurrentPlayer { get; private set; }
+        public Player Winner { get; private set; } = null;
+        public bool Draw { get; private set; } = false;
+
+        public Game() => CurrentPlayer = players.First();
+
+        public void MakeMove(int row, int column)
+        {
+            if (Winner != null)
+            {
+                return;
+            }
+
+            if (CurrentPlayer.MakeMove(row, column))
+            {
+                Winner = CurrentPlayer;
+                return;
+            }
+                
+            ++moveCount;
+
+            if (moveCount == MaxMoveCount)
+            {
+                Draw = true;
+                return;
+            }
+
+            CurrentPlayer = players[moveCount % players.Length];            
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Player.cs
@@ -0,0 +1,29 @@
+﻿
+namespace TicTacToe
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public char Symbol { get; private set; }
+        private bool[,] board = { {false, false, false }, { false, false, false },
+                { false, false, false } };
+
+        private bool IsWinner(int row, int column) => 
+                (board[row, 0] && board[row, 1] && board[row, 2]) ||
+                (board[0, column] && board[1, column] && board[2, column]) ||
+                (row == column && board[0, 0] && board[1, 1] && board[2, 2]) ||
+                (row + column == 2 && board[0, 2] && board[1, 1] && board[2, 0]);
+
+        public Player(string name, char symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
+
+        public bool MakeMove(int row, int column)
+        {
+            board[row, column] = true;
+            return IsWinner(row, column);
+        }
+    }
+}
