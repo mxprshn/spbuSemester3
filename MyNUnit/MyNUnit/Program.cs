d96ef4c5f3e28a3ea222a9ffe03f5f007fcbd43b
Max Parshin <43303110+mxprshn@users.noreply.github.com>
2019-12-09 00:31:31 +0300

    Added coloring library

--- MyNUnit/MyNUnit/Program.cs
+++ MyNUnit/MyNUnit/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pastel;
+using System.Drawing;
 
 namespace MyNUnit
 {
@@ -12,7 +14,7 @@ namespace MyNUnit
         {
             foreach (var test in TestRunner.Test(Console.ReadLine()))
             {
-                Console.WriteLine($"{test.ClassName} {test.Name} {test.IsPassed} {test.RunTime}");
+                Console.WriteLine($"{test.ClassName} {test.Name} {test.IsPassed} {test.RunTime}".Pastel("#27AE60"));
             }
             Console.ReadKey();
         }
