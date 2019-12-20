fb94891ee8882758e83d90d4132932f7d027401c
Max Parshin <43303110+mxprshn@users.noreply.github.com>
2019-12-20 21:30:16 +0300

    Removed usings

--- MyNUnit/MyNUnit/Program.cs
+++ MyNUnit/MyNUnit/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Pastel;
-using System.Drawing;
 
 namespace MyNUnit
 {
@@ -32,7 +27,7 @@ namespace MyNUnit
             }
             else
             {
-                Console.WriteLine($"{test.Name} in class {test.ClassName} is ignored: {test.IgnoreReason}.".Pastel("#F7DC6F"));
+                Console.WriteLine($"{test.Name} in class {test.ClassName} is ignored: {test.IgnoreReason}.";
             }
         }
     }
