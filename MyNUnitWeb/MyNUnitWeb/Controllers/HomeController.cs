e91089706bb457c2bd855ff217e2d01ab0aaef1b
mxprshn <mxprshn@gmail.com>
2019-12-21 14:13:41 +0300

    Added empty project

---
+++ MyNUnitWeb/MyNUnitWeb/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNUnitWeb.Models;
+
+namespace MyNUnitWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("TestRunner");
+        }
+
+        public IActionResult LoadAssembly(Assemblies ololo)
+        {
+            return View("TestRunner");
+        }
+
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
